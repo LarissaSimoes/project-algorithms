ce0b83cde89b3ec40447d53751692435c0e91a4c
Larissa Simões <larissacarneiro2@hotmail.com>
2023-08-22 14:40:25 -0300

    Reqs 1, 3, 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,53 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    middle = len(arr) // 2
+    left = arr[:middle]
+    right = arr[middle:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    left_index, right_index = 0, 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+
+    result.extend(left[left_index:])
+    result.extend(right[right_index:])
+    return result
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui!."""
-    raise NotImplementedError
+    def sorted_string(s):
+        return "".join(merge_sort(list(s.lower())))
+
+    if not first_string and not second_string:
+        return "", "", False
+
+    first_sorted = sorted_string(first_string)
+    second_sorted = sorted_string(second_string)
+
+    def char_count(s):
+        count = {}
+        for char in s:
+            count[char] = count.get(char, 0) + 1
+        return count
+
+    first_count = char_count(first_sorted)
+    second_count = char_count(second_sorted)
+
+    return first_sorted, second_sorted, first_count == second_count
+
+
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,18 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    
+    if permanence_period is None or any(
+        (start is None or not isinstance(start, int)) or 
+        (end is None or not isinstance(end, int)) or 
+        (end < start) 
+        for start, end in permanence_period
+    ):
+        return None
+    
+    count = 0
+    for start, end in permanence_period:
+        if start is not None and end is not None and start <= target_time <= end:
+            count += 1
+    
+    return count
